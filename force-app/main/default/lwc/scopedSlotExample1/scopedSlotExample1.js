2dbd031820477a603416c6b26322dee2dca9b6fd
Dennis James Matildo <dj.matildo@gmail.com>
2024-10-08 20:57:46 +0800

    Scoped Slot - Utilized the Child Component as a wrapper and the Parent as a Container.
    Wrapper term is usually used when designing poriton of a web design.

---
+++ force-app/main/default/lwc/scopedSlotExample1/scopedSlotExample1.js
@@ -0,0 +1,22 @@
+import { LightningElement } from "lwc";
+
+export default class ScopedSlotExample1 extends LightningElement {
+  item = [
+    {
+      id: "c8beb4d0-c712-47b7-960e-15e51d743e5d",
+      name: "Do irure excepteur nulla reprehenderit ea reprehenderit anim culpa labore culpa duis commodo officia nulla."
+    },
+    {
+      id: "170637aa-3659-47c3-8289-0d1f3703aaef",
+      name: "Cupidatat exercitation pariatur elit laborum occaecat qui do do."
+    },
+    {
+      id: "42ba41ac-c552-4d4f-ba1d-02c19514a2e7",
+      name: "Sunt incididunt reprehenderit ut occaecat ullamco nulla fugiat dolor in pariatur amet pariatur."
+    },
+    {
+      id: "a791df95-6f28-4a3c-846b-180f6ade4a46",
+      name: "Non ipsum in cillum consequat Lorem fugiat."
+    }
+  ];
+}
